58b7a38c4e51dd3c1a868d5faf35914686ac65b8
Andrei <Kulik>
2020-10-17 16:44:11 +0300

    Updates

---
+++ precache-manifest.b5ad7b9679c604adf54fab027b64482e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595a2c6985130d916a91690c5b78781a",
+    "url": "/TaskByLargeScaleSolutions/index.html"
+  },
+  {
+    "revision": "061ae4841bb194931c75",
+    "url": "/TaskByLargeScaleSolutions/static/css/main.f9b8bc68.chunk.css"
+  },
+  {
+    "revision": "9dce8742c14f2896a3af",
+    "url": "/TaskByLargeScaleSolutions/static/js/2.bd07b7f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TaskByLargeScaleSolutions/static/js/2.bd07b7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "061ae4841bb194931c75",
+    "url": "/TaskByLargeScaleSolutions/static/js/main.5e0f550c.chunk.js"
+  },
+  {
+    "revision": "06a335352c399de5f6e4",
+    "url": "/TaskByLargeScaleSolutions/static/js/runtime-main.0f153e1f.js"
+  }
+]);
